33e946e622989e3f0750bb3e6771e030d47b14b8
metastableB <donkdennis@gmail.com>
2015-05-26 23:09:36 +0530

    grep counting

--- scripts/grepCounting.sh
+++ scripts/grepCounting.sh
@@ -9,10 +9,16 @@
 
 numberOfPass=15
 level=1
+numberOfNodes=4847571
+total=0
 
 while [ $numberOfPass -gt 0 ] ; do
 	count=$(grep -e "GRAY" ./f1_r1_${level}/part* | grep -e "GRAY" -c )
 	echo "Level $level  : $count"
 	let	level=level+1
 	let numberOfPass=numberOfPass-1
+	let total=total+${count}
 done
+difference=${numberOfNodes}-${total}
+echo "total = " $total
+echo "difference = " $difference
