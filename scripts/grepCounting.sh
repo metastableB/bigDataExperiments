edc259cbafa5a506ce925ba72bd4d4bae285c19d
metastableB <donkdennis@gmail.com>
2015-05-30 04:48:59 +0530

    grep script calculates white and gray nodes as well

--- scripts/grepCounting.sh
+++ scripts/grepCounting.sh
@@ -7,10 +7,10 @@
 # This script is for printing the required grep outputs on all nodes level wise
 # For adaptation please make sure to change source path and number of inputs pass as required
 
-numberOfPass=15
+numberOfPass=9
 level=1
-numberOfNodes=4847571
-total=0
+numberOfNodes=81306
+total=1 # counting the source node
 
 while [ $numberOfPass -gt 0 ] ; do
 	count=$(grep -e "GRAY" ./f1_r1_${level}/part* | grep -e "GRAY" -c )
@@ -19,6 +19,8 @@ while [ $numberOfPass -gt 0 ] ; do
 	let numberOfPass=numberOfPass-1
 	let total=total+${count}
 done
-difference=${numberOfNodes}-${total}
-echo "total = " $total
-echo "difference = " $difference
+difference=$((numberOfNodes-total))
+echo "total of nodes on each level = " $total
+echo "difference with the total no of nodes = " $difference
+echo "Total Black Nodes at the end = " $(grep -P 'BLACK' ./f1_r1_$((level-1))/part* | grep -P 'BLACK' -c)
+echo "Total WHITE nodes at the end = " $(grep -P 'WHITE' ./f1_r1_$((level-1))/part* | grep -P 'WHITE' -c)
