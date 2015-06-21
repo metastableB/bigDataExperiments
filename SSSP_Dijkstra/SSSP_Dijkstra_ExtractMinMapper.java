2211f9452521e10910d6c10fd71a8221ff08d6ec
metastableB <donkdennis@gmail.com>
2015-06-21 07:47:43 +0530

    Dijkstra extract min mapper and reducer

---
+++ SSSP_Dijkstra/SSSP_Dijkstra_ExtractMinMapper.java
@@ -0,0 +1,36 @@
+ /*
+ * Don K Dennis (metastableB)
+ * 19 June 2015
+ * donkdennis [at] gmail.com
+ *
+ * Finding the SSSP using Dijkstra 
+ * Inp: source<tab>distance|color|parent|adjacency list with wights (csv)
+ * Out: source<tab>distance|color|parent|<adj list>
+ *
+ * (c) IIIT Delhi, 2015
+ */
+
+import java.io.IOException;
+import java.util.*;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Mapper.Context;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.LongWritable;
+
+// The type parameters are the input keys type, the input values type, the
+// output keys type, the output values type
+public class SSSP_Dijkstra_ExtractMinMapper extends Mapper<LongWritable, Text, Text, Text> {
+
+    @Override
+    public void map(LongWritable key, Text value, Context context)
+            throws IOException, InterruptedException {
+
+        Node inNode = new Node(value.toString());
+        Long distance = inNode.getDistance();
+        context.write(new Text(1),new Text(distance.toString() + "," + inNode.getId()));
+    }
+}
+
+
+
---
+++ SSSP_Dijkstra/SSSP_Dijkstra_ExtractMinReducer.java
@@ -0,0 +1,40 @@
+ /*
+ * Don K Dennis (metastableB)
+ * 19 June 2015
+ * donkdennis [at] gmail.com
+ *
+ * Finding the SSSP using Dijkstra 
+ * Inp: source<tab>distance|color|parent|adjacency list with wights (csv)
+ * Out: source<tab>distance|color|parent|<adj list>
+ *
+ * (c) IIIT Delhi, 2015
+ */
+
+
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.io.Text;
+import java.io.IOException;
+
+public class SSSP_Dijkstra_ExtractMinReducer extends Reducer<Text, Text, Text, Text> {
+     @Override
+    public void reduce(Text key, Iterable<Text> values, Context context)
+            throws IOException, InterruptedException {
+        Integer max = Integer.MAX_VALUE;  
+        String id;       
+        for (Text value : values) {
+            String[] tokens = value.split(",");
+            Integer distance = Integer.valueOf(tokens[0]);
+            
+            if(max < distance) {
+            	max = distance;
+            	id = tokens[1];
+            }
+        }
+        context.write(new Text(id),new Text(""));      
+    }
+}
+
+
+
+
+
